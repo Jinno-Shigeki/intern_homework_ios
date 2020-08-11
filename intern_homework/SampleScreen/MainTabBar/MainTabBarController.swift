68f71a3b5c13d690d5aa2338ced2b8b6167b9b48
jinn <booboo080yamaya@gmail.com>
2020-08-11 19:33:04 +0900

    課題3,4,5作成

--- intern_homework/SampleScreen/MainTabBar/MainTabBarController.swift
+++ intern_homework/SampleScreen/MainTabBar/MainTabBarController.swift
@@ -10,10 +10,11 @@ final class MainTabBarController: UITabBarController {
 
     private func setTab() {
         let searchViewController: UINavigationController = R.storyboard.search.instantiateInitialViewController()!
-
+        searchViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.search, tag: 1)
     // 仮ViewController。必要に応じて置き換えてください。
         let dummyViewController = UIViewController()
-
+        dummyViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.contacts, tag: 2)
+        
         setViewControllers([searchViewController, dummyViewController], animated: false)
     }
 }
